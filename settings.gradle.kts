1bbb30d19b1b2cc6cf31c8506cf30ce6f80d2b2f
geonmo9 <126770432+geonmo9@users.noreply.github.com>
2024-11-13 11:19:49 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TradeFunc"
+include(":app")
